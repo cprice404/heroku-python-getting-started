b8204d2bf155951cd1d4db9beccdc05f46686130
Chris Price <chris@puppetlabs.com>
2017-03-12 19:56:37 -0700

    Refactor to only show people from one tag at a time

--- hello/view_helpers/tags.py
+++ hello/view_helpers/tags.py
@@ -1,9 +1,17 @@
+import urllib
+
 from django.shortcuts import render
 from hello.utils.oauth import get_oauth_service
 
 
 def handle_tags_request(request):
-    tag_prefix = request.POST.get('tag_prefix')
+    tag_prefix = None
+    selected_tag = None
+    if request.method == 'POST':
+        tag_prefix = request.POST.get('tag_prefix')
+    elif request.method == 'GET':
+        tag_prefix = request.GET.get('tag_prefix')
+        selected_tag = request.GET.get('selected_tag')
 
     nation_slug = request.session['nation_slug']
     service = get_oauth_service(nation_slug)
@@ -17,15 +25,18 @@ def handle_tags_request(request):
 
     filtered_tags = []
     if tag_prefix:
-        filtered_tags = filter(lambda x: x.lower().startswith(tag_prefix.lower()),
-                               tags)
+        def to_tag_dict(tag):
+            return {'raw': tag, 'encoded': urllib.quote_plus(tag)}
+
+        filtered_tags = map(to_tag_dict,
+                            filter(lambda x: x.lower().startswith(tag_prefix.lower()),
+                                   tags))
 
     matching_people = []
-    filtered_tag_index = 0
-    while (len(matching_people) < 10) and (filtered_tag_index < len(filtered_tags)):
+    if selected_tag:
         url = "https://" + nation_slug + \
               ".nationbuilder.com/api/v1/tags/" + \
-              filtered_tags[filtered_tag_index] + \
+              selected_tag + \
               "/people"
         people_response = session.get(url,
                                       params={'format': 'json'},
@@ -33,10 +44,10 @@ def handle_tags_request(request):
         people = people_response.json()['results']
         for person in people:
             matching_people.append(person['first_name'] + " " + person['last_name'])
-        filtered_tag_index += 1
 
     return render(request, 'tags.html', context={'tags': tags,
                                                  'tag_prefix': tag_prefix or '',
                                                  'filtered_tags': filtered_tags,
+                                                 'selected_tag': selected_tag,
                                                  'matching_people': matching_people})
 
