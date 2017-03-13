1889f9ef0498f8c2359cb759b1c38f3a9c0de787
Chris Price <chris@puppetlabs.com>
2017-03-12 21:29:04 -0700

    Added edit button, basic form.  Save doesn't work yet.

--- hello/view_helpers/tags.py
+++ hello/view_helpers/tags.py
@@ -7,11 +7,17 @@ from hello.utils.oauth import get_oauth_service
 def handle_tags_request(request):
     tag_prefix = None
     selected_tag = None
+    edit_person_id = None
     if request.method == 'POST':
         tag_prefix = request.POST.get('tag_prefix')
+        selected_tag = request.POST.get('selected_tag')
+        edit_person_id = request.POST.get('edit_person_id')
+        save_person(edit_person_id, request)
+        edit_person_id = None
     elif request.method == 'GET':
         tag_prefix = request.GET.get('tag_prefix')
         selected_tag = request.GET.get('selected_tag')
+        edit_person_id = request.GET.get('edit_person_id')
 
     nation_slug = request.session['nation_slug']
     service = get_oauth_service(nation_slug)
@@ -33,6 +39,7 @@ def handle_tags_request(request):
                                    tags))
 
     matching_people = []
+    selected_tag_map = None
     if selected_tag:
         url = "https://" + nation_slug + \
               ".nationbuilder.com/api/v1/tags/" + \
@@ -43,21 +50,31 @@ def handle_tags_request(request):
                                       headers={'content-type': 'application/json'})
         people = people_response.json()['results']
         for person in people:
-            print "PERSON KEYS: '%s'" % person.keys()
-            print "PERSON TAGS: '%s'" % person['tags']
+            # print "PERSON KEYS: '%s'" % person.keys()
+            # print "PERSON TAGS: '%s'" % person['tags']
+            # print "PERSON ID: '%s'" % person['id']
+            # print "PERSON: '%s'" % person
             team_tags = filter(lambda t: t.lower().startswith("team"),
                                person['tags'])
-            person_map = {'first_name': person['first_name'],
+            person_map = {'id': str(person['id']),
+                          'first_name': person['first_name'],
                           'last_name': person['last_name'],
                           'team_tags': team_tags,
                           'email': person['email'],
                           'phone': person['phone'],
                           'address': person['primary_address']}
             matching_people.append(person_map)
+            # print("EDIT_PERSON_ID: '%s'" % edit_person_id)
+        selected_tag_map = {'raw': selected_tag,
+                            'encoded': urllib.quote_plus(selected_tag)}
 
     return render(request, 'tags.html', context={'tags': tags,
                                                  'tag_prefix': tag_prefix or '',
                                                  'filtered_tags': filtered_tags,
-                                                 'selected_tag': selected_tag,
+                                                 'selected_tag': selected_tag_map,
+                                                 'edit_person_id': edit_person_id,
                                                  'matching_people': matching_people})
 
+def save_person(person_id, request):
+    print("SAVING PERSON: '%s'" % request.POST)
+    pass
